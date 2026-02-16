f877d97d31b736ab477445994a37d90d96b82961
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-16 10:04:18 +0000

    add ExpressionCalculator input-output basics

---
+++ ExpresssionCalculator.java
@@ -0,0 +1,20 @@
+package oops_experiment1;
+
+public class ExpresssionCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   int result1=(101+0) / 3;
+		   System.out.println("Result of (101+0) / 3:" + result1); //to print
+		   double result2=(3.0e-6 * 100000000.1);
+		   System.out.println("Result of (3.0e-6 * 100000000.1):" +result2);
+		   boolean result3=(true && true);
+		   System.out.println("Result of (true&&true):" +result3);
+		   boolean result4=(false && true);
+		   System.out.println("Result of(false&&true):" +result4);
+		   boolean result5=((false && false)||(true && true));
+		   System.out.println("Result of (false && false)||(true && true) :" +result5);
+	}
+		   
+	}
+
